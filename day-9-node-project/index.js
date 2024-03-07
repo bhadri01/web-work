a96dba340969f2937da1e5966c13f8e67f96536d
bhadri2002 <bhadri2002@gmail.com>
2024-03-07 16:54:53 +0530

    updated

--- day-9-node-project/index.js
+++ day-9-node-project/index.js
@@ -1,14 +1,39 @@
 const express = require("express");
-const { Loginfunction, Rootmethod,UserFunction } = require("./lib/routeFunction");
+const {
+  Loginfunction,
+  Rootmethod,
+  UserFunction,
+  signupFunction,
+} = require("./lib/routeFunction");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "demo",
+});
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to the database");
+});
+
 const app = express();
+
+app.use(function(request, response, next){
+  request.db = db;
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", Rootmethod);
 app.post("/login", Loginfunction);
-app.get("/user",UserFunction)
+app.post("/signup", signupFunction);
+app.get("/user", UserFunction);
 
 const PORT = 8000;
 
--- day-9-node-project/lib/routeFunction.js
+++ day-9-node-project/lib/routeFunction.js
@@ -13,12 +13,42 @@ function Loginfunction(request, response) {
 }
 
 function UserFunction(request, response) {
-  console.log(request.query)
-  response.send("user data")
+  console.log(request.query);
+  response.send("user data");
+}
+
+function signupFunction(request, response) {
+  const { username, email, password, confirmPassword } = request.body;
+  let noUsers = 0;
+  // check for required fields
+  let sql = `SELECT * FROM users WHERE username='${username}' and email='${email}'`;
+  request.db.query(sql, function (err, result) {
+    if (result.length > 0) {
+      noUsers = 1;
+    }
+    if (noUsers == 0) {
+      // insert part
+      if (password == confirmPassword) {
+        const sql = `INSERT INTO users (username, password, email) VALUES ('${username}','${password}','${email}')`;
+        request.db.query(sql, function (err, result) {
+          if (err) {
+            response.send(err);
+          } else {
+            response.send("signup success");
+          }
+        });
+      } else {
+        response.send("signup failure incorrect password and confirm password");
+      }
+    } else {
+      response.send("username or email already exist");
+    }
+  });
 }
 
 module.exports = {
   Rootmethod,
   Loginfunction,
   UserFunction,
+  signupFunction,
 };
